5434a0b4d64d357f7c8c415fcd3c0f18cb948042
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-26 16:41:41 -0300

    Restaurant databiding.

--- src/app/restaurantes/restaurantes.component.ts
+++ src/app/restaurantes/restaurantes.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Oferta } from '../shared/oferta.model'
 import { OfertasService} from '../ofertas.service'
+
 @Component({
   selector: 'app-restaurantes',
   templateUrl: './restaurantes.component.html',
   styleUrls: ['./restaurantes.component.css'],
   providers: [ OfertasService ]
 })
+
 export class RestaurantesComponent implements OnInit {
+
+  public ofertas: Oferta[]
 
   constructor(private ofertasService: OfertasService) { }
 
   ngOnInit(): void {
     this.ofertasService.getOfertasPorCategoria('restaurante')
       .then((ofertas: Oferta[]) => {
-        console.log(ofertas)
+        this.ofertas = ofertas
       })
   }
 
