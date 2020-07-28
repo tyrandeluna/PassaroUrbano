1e4f2be5c2ed3ec5194382b8fdf7d4434e11a4d9
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-28 10:39:38 -0300

    HTTP Request with promise.

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -1,20 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router'
+import { Oferta } from '../shared/oferta.model'
+import { OfertasService } from '../ofertas.service'
 
 @Component({
   selector: 'app-oferta',
   templateUrl: './oferta.component.html',
-  styleUrls: ['./oferta.component.css']
+  styleUrls: ['./oferta.component.css'],
+  providers: [ OfertasService ]
 })
 export class OfertaComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) {}
+  public oferta: Oferta
+
+  constructor(private route: ActivatedRoute, private ofertasService: OfertasService) {}
 
   ngOnInit(): void {
-    //this.route.snapshot.params['id']
-    this.route.params.subscribe((parametro: any) => {
-      console.log(parametro)
-    })
+    this.ofertasService.getOfertasPorId(this.route.snapshot.params['id'])
+      .then((oferta: Oferta) => {
+        this.oferta = oferta
+      })
   }
 
 }
--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -20,4 +20,10 @@ export class OfertasService {
             .toPromise()
             .then((resposta: any) => resposta)
     }
+
+    public getOfertasPorId(id: number): Promise<Oferta>{
+        return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+        .toPromise()
+        .then((resposta: any) => resposta.shift())
+    }
 }
