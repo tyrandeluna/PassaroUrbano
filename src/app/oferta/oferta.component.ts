52c4642855f273e31e58083febf1bafda1ba399a
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-28 10:20:10 -0300

    Routes with Subscribe.

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -11,7 +11,10 @@ export class OfertaComponent implements OnInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit(): void {
-    this.route.snapshot.params['id']
+    //this.route.snapshot.params['id']
+    this.route.params.subscribe((parametro: any) => {
+      console.log(parametro)
+    })
   }
 
 }
