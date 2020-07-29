cf08966120bf93e07592b75aad1a91589c5dd2d0
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-29 10:20:07 -0300

    Recovering parent params.

--- src/app/oferta/ond-fica/ond-fica.component.ts
+++ src/app/oferta/ond-fica/ond-fica.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-ond-fica',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OndFicaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.parent.snapshot.params['id']
   }
 
 }
