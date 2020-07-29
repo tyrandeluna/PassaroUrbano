f091b969e2ae3e3565075771a3486e0cbaed1fa6
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-29 09:45:12 -0300

    "ComoUsar" and "OndeFica" components.

---
+++ src/app/oferta/ond-fica/ond-fica.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ond-fica',
+  templateUrl: './ond-fica.component.html',
+  styleUrls: ['./ond-fica.component.css']
+})
+export class OndFicaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
