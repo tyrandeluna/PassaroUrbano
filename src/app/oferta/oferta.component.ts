6d0e2b4bce603a8b4e55efd27886de53c8b02bcb
MariaBeatriz <mb.almeidadv@hotmail.com>
2020-07-30 10:42:55 -0300

    Observables part 4. Error() and Complete()

--- src/app/oferta/oferta.component.ts
+++ src/app/oferta/oferta.component.ts
@@ -37,11 +37,15 @@ export class OfertaComponent implements OnInit {
     let meuObservableTeste = Observable.create((observer: Observer<number>) =>{
       observer.next(1)
       observer.next(3)
+      observer.complete()
+      observer.next(3)
     })
 
     //observable (observador)
     meuObservableTeste.subscribe(
-      (resultado: number) => console.log(resultado + 10)
+      (resultado: number) => console.log(resultado + 10),
+      (erro: string) => console.log(erro),
+      () => console.log('Stream de eventos foi finalizada')
     )
   }
 
